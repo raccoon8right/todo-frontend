e84f287208453854d62f5cf0c13a8839bcfa35bf
raccoon8right <gpepito408@gmail.com>
2026-05-13 03:30:51 -0400

    fix: use HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Tareas from './pages/Tareas'
@@ -7,13 +7,13 @@ import ProtectedRoute from './components/ProtectedRoute'
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/tareas" element={<ProtectedRoute><Tareas /></ProtectedRoute>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
